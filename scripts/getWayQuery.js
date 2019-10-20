1f5481eb35ded5523a0912d3dbcaae07118a9d52
maxim218 <max12345@yandex.ru>
2019-10-21 01:27:09 +0300

    Render result array

--- scripts/getWayQuery.js
+++ scripts/getWayQuery.js
@@ -16,6 +16,18 @@ export default function getWayQuery(wwwObject, dictionary, canvasManager, otrArr
             alert("Ошибка при HTTP запросе");
         } else {
             log("Ответ от сервера со статусом 200");
+            const arr = JSON.parse(value).arr;
+            renderResultArray(arr);
         };
     });
+
+    function renderResultArray(arr) {
+        for(let i = 0; i < arr.length; i++) {
+            if(i + 1 < arr.length) {
+                const A = arr[i];
+                const B = arr[i + 1];
+                canvasManager.drawLine(A.Px, A.Py, B.Px, B.Py, "#FF0000");
+            }
+        }
+    }
 }
